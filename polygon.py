08a40a9acba79f5b8a1cef78ec63af2f38b08192
Jennifer Klay <jklay@calpoly.edu>
2012-04-05 16:21:11 -0700

    Ch 4 work with swampy

---
+++ polygon.py
@@ -0,0 +1,18 @@
+#
+# ThinkPython Chapter 4
+#
+# J.L.Klay
+# 03-Apr-2012
+#
+
+from TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+
+wait_for_user()
